f502bd957ba22f2a5355444de3c9125c24811e4f
yaya <flora24ti@mahasiswa.pcr.ac.id>
2025-10-13 16:09:29 +0700

    Template Admin - Volt

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,5 @@ Route::get('/home', [HomeController::class, 'index']) -> name(name:'home');
 
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
+
+Route::get('dashboard', [DashboardController::class, 'index']) -> name(name:'dashboard');
